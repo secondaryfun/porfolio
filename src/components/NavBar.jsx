abe1b588de0c0125685417cba1f7d621017aff05
secondaryfun <c.birkenhagen@gmail.com>
2020-07-21 23:10:10 -0400

    Debugging pass.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import {
-    BrowserRouter as Router,
-    Link,
-} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './NavBar.css';
 
 import logoName from "../assets/images/icons/logo-name-black.svg"
@@ -11,7 +8,7 @@ function NavBar() {
 
     return (
         <nav className="nav-wrapper flex-row hide-on-small">
-            <img alt="" src={logoName} alt="Christopher Birkenhagen" className="nav__logo-name" />
+            <img src={logoName} alt="Christopher Birkenhagen" className="nav__logo-name" />
             <div className="nav__link-wrapper nav-bar-margins flex-row hide-on-small">
                 <Link className="nav__link " to="/" >HOME</Link>
                 <Link className="nav__link " to="/skills" >SKILLS</Link>
