bd7a5d137a935d3ced577dbe521bbed6737972cc
secondaryfun <c.birkenhagen@gmail.com>
2020-07-26 16:01:51 -0400

    no time.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,61 @@
 import React from 'react';
+
+import AwesomeSlider from "react-awesome-slider"
+import 'react-awesome-slider/dist/styles.css';
+
 import {
-	BrowserRouter as Router,
-	Route,
-	Switch,
-	Redirect,
-} from 'react-router-dom';
-import './App.css';
-import './services/utilities.css';
-import Home from './pages/Home';
-import Skills from './pages/Skills';
-import Projects from './pages/Projects';
-import ProjectDetail from './pages/ProjectDetail';
-import Resume from './pages/Resume';
+    Provider,
+    Link,
+    withNavigationContext,
+    withNavigationHandlers
+} from "react-awesome-slider/dist/navigation";
 
+// Wrapp the AwesomeSlider component with the navigationHandlers
+const NavigationSlider = withNavigationHandlers(AwesomeSlider);
 
-function App(props) {
-
-	return (
-		<Router>
-			<Switch>
-				<Redirect path="/" exact to="home" />
-				<Route path="/home" exact component={Home} />
-				<Route path="/skills" exact component={Skills} />
-				<Route path="/projects" exact component={Projects} />
-				<Route path="/project-detail" exact component={ProjectDetail} />
-				<Route path="/resume" exact component={Resume} />
-			</Switch>
-		</Router>
-
-
-	);
+// Create an AwesomeSlider instance with some content
+const Slider = () => {
+    return (
+        <NavigationSlider
+            className="awesome-slider"
+            media={[
+                {
+                    slug: "page-one",
+                    className: "page-one",
+                    children: () => <p>Page One</p>
+                },
+                {
+                    slug: "page-two",
+                    className: "page-two",
+                    children: () => <p>Page Two</p>
+                }
+            ]}
+        />
+    )
 }
 
-export default App;
+// Page header navigation
+const Header = () => {
+    return (
+        <Header>
+            <nav>
+                <Link href="page-one">Page One</Link>
+                <Link href="page-two">Page Two</Link>
+            </nav>
+        </Header>
+    )
+}
+
+// Wrapp the aplication with the navigation Provider passing down the current page slug.
+const App = () => {
+    const slug = "[THE INITIAL RENDERED SLUG]";
+
+    return (
+        <Provider slug={slug}>
+            <Header />
+            <NavigationSlider />
+        </Provider>
+    )
+}
+
+export default App
