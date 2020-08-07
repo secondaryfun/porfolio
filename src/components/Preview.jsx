bfc00e650121231c29387e501948c128e7d62650
secondaryfun <c.birkenhagen@gmail.com>
2020-08-06 23:57:37 -0400

    animated previews bug fix

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -7,24 +7,24 @@ import arrow from "../assets/images/icons/arrow-button-white.svg"
 function Preview(props) {
     const project = props.project
     return (
-        <Link to={
-            {
+        <div className={`Preview__animate ${props.className}`}>
+            <Link to={{
                 pathname: `/project-detail`,
                 state: { project }
-            }
-        }
-            className={`open-sans headline Preview__link ${props.className}`}
-        >
-            <div className={`Preview-wrapper flex-column space-around `}>
-                <h2 className="oswald">{props.title}</h2>
-                <p>
-                    <span className="Preview__nav-text white">
-                        view project
-                </span>
-                    <img src={arrow} alt="arrow" className="Preview__img" />
-                </p>
-            </div>
-        </Link >
+            }}
+                className={`open-sans headline Preview__link `}
+            >
+                <div className={`Preview-wrapper flex-column space-around `}>
+                    <h2 className="oswald Preview__title">{props.title}</h2>
+                    <p>
+                        <span className="Preview__nav-text white">
+                            view project
+                    </span>
+                        <img src={arrow} alt="arrow" className="Preview__img" />
+                    </p>
+                </div>
+            </Link >
+        </div>
     );
 }
 
